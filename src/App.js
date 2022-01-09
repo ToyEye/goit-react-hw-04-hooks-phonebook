fbd21d9a36d3a83cf1af5b15d2e9299fbcf9f66d
Korotenko Alex <alexey.korotenko90@gmail.com>
2022-01-09 21:57:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Container } from './Components/Container';
 import Form from './Components/Form';
 import { Section, Title } from './Components/Section';
@@ -8,19 +8,25 @@ import { nanoid } from 'nanoid';
 import Filter from './Components/Filter';
 import toast, { Toaster } from 'react-hot-toast';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+// const contacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
-  addContact = ({ name, number }) => {
-    let array = this.state.contacts.map(contact => contact.name);
+export default function App() {
+  const contact = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  const [contacts, setContacts] = useState(contact);
+  const [filter, setFilter] = useState('');
+
+  const addContact = ({ name, number }) => {
+    let array = contacts.map(contact => contact.name);
     if (!array.includes(name)) {
       const newContact = {
         id: nanoid(),
@@ -28,7 +34,7 @@ class App extends Component {
         number: number,
       };
       toast.success('Контакт добавлен');
-      return this.setState(({ contacts }) => ({
+      return setContacts(({ contacts }) => ({
         contacts: [newContact, ...contacts],
       }));
     } else {
@@ -37,51 +43,119 @@ class App extends Component {
     }
   };
 
-  deleteItem = itemId => {
-    this.setState(prevState => ({
+  const deleteItem = itemId => {
+    setContacts(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== itemId),
     }));
   };
 
-  filterEnter = evt => {
-    this.setState({ filter: evt.target.value });
+  const filterEnter = evt => {
+    setFilter({ filter: evt.target.value });
   };
-
-  filterChange = () => {
-    const { contacts, filter } = this.state;
-    const normalizeFilter = filter.toLocaleLowerCase();
+  const filterChange = () => {
+    const normalizeFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(normalizeFilter)
+      contact.name.toLowerCase().includes(normalizeFilter)
     );
   };
 
-  render() {
-    const { filter } = this.state;
-    return (
-      <Container>
-        <Toaster
-          toastOptions={{
-            error: {
-              duration: 2000,
-            },
-          }}
-        />
-        <Section>
-          <Title>Phonebook</Title>
-          <Form onSubmit={this.addContact} />
-        </Section>
-        <Section>
-          <Title>Contacts</Title>
-          <Filter value={filter} onChange={this.filterEnter} />
-          <ContactList
-            contacts={this.filterChange()}
-            onDeleteItem={this.deleteItem}
-          />
-        </Section>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Toaster
+        toastOptions={{
+          error: {
+            duration: 2000,
+          },
+        }}
+      />
+      <Section>
+        <Title>Phonebook</Title>
+        <Form onSubmit={addContact} />
+      </Section>
+      <Section>
+        <Title>Contacts</Title>
+        <Filter value={filter} onChange={filterEnter} />
+        <ContactList contacts={filterChange()} onDeleteItem={deleteItem} />
+      </Section>
+    </Container>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+// contacts: [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ],
+//     filter: '',
+//   };
+
+// addContact = ({ name, number }) => {
+//   let array = this.state.contacts.map(contact => contact.name);
+//   if (!array.includes(name)) {
+//     const newContact = {
+//       id: nanoid(),
+//       name: name,
+//       number: number,
+//     };
+//     toast.success('Контакт добавлен');
+//     return this.setState(({ contacts }) => ({
+//       contacts: [newContact, ...contacts],
+//     }));
+//   } else {
+//     toast.error('Контакт существует!');
+//     return;
+//   }
+// };
+
+// deleteItem = itemId => {
+//   this.setState(prevState => ({
+//     contacts: prevState.contacts.filter(contact => contact.id !== itemId),
+//   }));
+// };
+
+// filterEnter = evt => {
+//   this.setState({ filter: evt.target.value });
+// };
+
+// filterChange = () => {
+//   const { contacts, filter } = this.state;
+//   const normalizeFilter = filter.toLowerCase();
+
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizeFilter)
+//   );
+// };
+
+//   render() {
+//     const { filter } = this.state;
+// return (
+//   <Container>
+//     <Toaster
+//       toastOptions={{
+//         error: {
+//           duration: 2000,
+//         },
+//       }}
+//     />
+//     <Section>
+//       <Title>Phonebook</Title>
+//       <Form onSubmit={this.addContact} />
+//     </Section>
+//     <Section>
+//       <Title>Contacts</Title>
+//       <Filter value={filter} onChange={this.filterEnter} />
+//       <ContactList
+//         contacts={this.filterChange()}
+//         onDeleteItem={this.deleteItem}
+//       />
+//     </Section>
+//   </Container>
+// );
+//   }
+// }
+
+// export default App;
--- src/Components/Form/Form.styled.js
+++ src/Components/Form/Form.styled.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import PropTepes from 'prop-types';
+// import PropTepes from 'prop-types';
 import Button from '../Button';
 import { ImputEnter, InputType, InputText } from '../FormComponents';
 
@@ -15,66 +15,132 @@ const FormStyled = styled.form`
   border-radius: 5px;
 `;
 
-class Form extends Component {
-  static propTepes = {
-    onSubmit: PropTepes.func.isRequired,
-  };
+export default function Form({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  state = {
-    name: '',
-    number: '',
-  };
-
-  handleChange = evt => {
+  const handleChange = evt => {
     const { name, value } = evt.target;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
-  render() {
-    return (
-      <FormStyled onSubmit={this.handleSubmit}>
-        <InputType>
-          {' '}
-          <InputText>Name</InputText>
-          <ImputEnter
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            placeholder="Enter your name"
-            required
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </InputType>
-        <InputType>
-          {' '}
-          <InputText>Name</InputText>
-          <ImputEnter
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            placeholder="Enter your number"
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-        </InputType>
-        <Button type="submit">Add contact</Button>
-      </FormStyled>
-    );
-  }
+
+  return (
+    <FormStyled onSubmit={handleSubmit}>
+      <InputType>
+        {' '}
+        <InputText>Name</InputText>
+        <ImputEnter
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          placeholder="Enter your name"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </InputType>
+      <InputType>
+        {' '}
+        <InputText>Name</InputText>
+        <ImputEnter
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="Enter your number"
+          value={number}
+          onChange={handleChange}
+        />
+      </InputType>
+      <Button type="submit">Add contact</Button>
+    </FormStyled>
+  );
 }
 
-export default Form;
+// class Form extends Component {
+//   static propTepes = {
+//     onSubmit: PropTepes.func.isRequired,
+//   };
+
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+// handleChange = evt => {
+//   const { name, value } = evt.target;
+//   this.setState({ [name]: value });
+// };
+
+// handleSubmit = evt => {
+//   evt.preventDefault();
+
+//   this.props.onSubmit(this.state);
+//   this.reset();
+// };
+
+// reset = () => {
+//   this.setState({ name: '', number: '' });
+// };
+//   render() {
+// return (
+//   <FormStyled onSubmit={this.handleSubmit}>
+//     <InputType>
+//       {' '}
+//       <InputText>Name</InputText>
+//       <ImputEnter
+//         type="text"
+//         name="name"
+//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//         placeholder="Enter your name"
+//         required
+//         value={this.state.name}
+//         onChange={this.handleChange}
+//       />
+//     </InputType>
+//     <InputType>
+//       {' '}
+//       <InputText>Name</InputText>
+//       <ImputEnter
+//         type="tel"
+//         name="number"
+//         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//         required
+//         placeholder="Enter your number"
+//         value={this.state.number}
+//         onChange={this.handleChange}
+//       />
+//     </InputType>
+//     <Button type="submit">Add contact</Button>
+//   </FormStyled>
+// );
+//   }
+// }
+
+// export default Form;
